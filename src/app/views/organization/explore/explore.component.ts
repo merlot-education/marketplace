929f04a0f19671a4b44275ef63e750b03054c2fa
Marc B <125284318+M-Busk@users.noreply.github.com>
2023-03-03 15:44:17 +0100

    improved explore and edit organization

--- src/app/views/organization/explore/explore.component.ts
+++ src/app/views/organization/explore/explore.component.ts
@@ -25,14 +25,14 @@ export class ExploreComponent implements OnInit {
       registrationNumber: "0762747721",
       registrationType: "local",
       countryCode: "BE",
-      postalCode: "12345",
-      street: "asdasd",
+      postalCode: "1210 BRU",
+      street: "Avenue des Arts 6-9",
       iconName: "cil-aperture"
     },
     {
       name: "Dataport AöR",
       merlotId: "000002",
-      registrationNumber: "",
+      registrationNumber: "0762747722",
       registrationType: "local",
       countryCode: "D",
       postalCode: "24161 Altenholz",
