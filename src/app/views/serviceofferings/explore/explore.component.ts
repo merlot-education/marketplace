7fa9f93b42115916280bae9357e6a05dbcdccecd
Marc B <125284318+M-Busk@users.noreply.github.com>
2023-03-06 13:57:22 +0100

    service offerings initial design

--- src/app/views/serviceofferings/explore/explore.component.ts
+++ src/app/views/serviceofferings/explore/explore.component.ts
@@ -1,10 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface IOfferings {
+  name: string,
+  country: string,
+  provider: string,
+  availablesince: string,
+}
+
 @Component({
   templateUrl: './explore.component.html',
   styleUrls: ['./explore.component.scss']
 })
 export class ExploreComponent implements OnInit {
+
+  offerings: IOfferings[] = [
+    {
+      name: "DataOffering1",
+      country: "De",
+      provider: "ProviderCorp",
+      availablesince: "Jan 1, 2021"
+    },
+    {
+      name: "DataOffering2",
+      country: "De",
+      provider: "ProviderCorp",
+      availablesince: "Jan 1, 2021"
+    },
+    {
+      name: "SmartServiceOffering1",
+      country: "De",
+      provider: "ProviderCorp",
+      availablesince: "Jan 1, 2021"
+    }
+  ]
 
   constructor() {
   }
