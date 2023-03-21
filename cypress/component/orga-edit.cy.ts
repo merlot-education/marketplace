f9ead5bf8de9dd916fc281f6908964297a14fbce
Marc B <125284318+M-Busk@users.noreply.github.com>
2023-03-21 17:12:45 +0100

    adding cypress with demo tests

---
+++ cypress/component/orga-edit.cy.ts
@@ -0,0 +1,7 @@
+import { EditComponent } from '../../src/app/views/organization/edit/edit.component'
+
+describe('orga-edit.cy.ts', () => {
+  it('mounts', () => {
+    cy.mount(EditComponent)
+  })
+})
---
+++ cypress/e2e/merlot-test.cy.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+context('Actions', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:4200/')
+  })
+
+  it('.click() - click on a DOM element', () => {
+    // https://on.cypress.io/click
+    cy.get('#login-button').click()
+    cy.get('#role-select').should('be.visible').select(1, {force: true})
+    cy.get('#role-select').should('have.value', 'Prokurist für Nachhilfeclub')
+    cy.get('#role-select').should('not.have.value', 'Prokurist für Dataport')
+    cy.get('#logout-button').click()
+  })
+
+  
+})
