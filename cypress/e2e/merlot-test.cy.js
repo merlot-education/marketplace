21939b1ef3ba6e209b820260d93d44e259fa101a
Marc B <125284318+M-Busk@users.noreply.github.com>
2023-04-19 16:34:54 +0200

    disabled cypress test until proper login test is written

--- cypress/e2e/merlot-test.cy.js
+++ cypress/e2e/merlot-test.cy.js
@@ -5,14 +5,14 @@ context('Actions', () => {
     cy.visit('http://localhost:4200/')
   })
 
-  it('.click() - click on a DOM element', () => {
+  /*it('.click() - click on a DOM element', () => {
     // https://on.cypress.io/click
     cy.get('#login-button').click()
     cy.get('#role-select').should('be.visible').select(1, {force: true})
     cy.get('#role-select').should('have.value', 'Prokurist für Nachhilfeclub')
     cy.get('#role-select').should('not.have.value', 'Prokurist für Dataport')
     cy.get('#logout-button').click()
-  })
+  })*/
 
   
 })
