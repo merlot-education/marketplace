434704f304edad7695d93752a27235cd0109e47c
Marc B <125284318+M-Busk@users.noreply.github.com>
2023-05-15 16:37:14 +0200

    changed sso urls to reflect split between dev and demo

--- src/environments/environment.demo.ts
+++ src/environments/environment.demo.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    sso_url: "https://sso.common.merlot-education.eu/",
+    sso_url: "https://sso.demo.merlot-education.eu/",
     aaam_api_url: "https://api.demo.merlot-education.eu/aaam",
     organizations_api_url: "https://api.demo.merlot-education.eu/organisations"
   };
