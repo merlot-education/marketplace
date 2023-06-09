c7316cb9daca1cc9674f664eed1d1e9da756e484
Marc B <125284318+M-Busk@users.noreply.github.com>
2023-06-09 11:47:31 +0200

    updated api urls

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -4,5 +4,6 @@ export const environment = {
     aaam_api_url: "https://api.dev.merlot-education.eu/aaam/",
     organizations_api_url: "https://api.dev.merlot-education.eu/organisations/",
     serviceoffering_api_url: "https://api.dev.merlot-education.eu/serviceoffering/",
+    contract_api_url: "https://api.dev.merlot-education.eu/contract/",
     wizard_api_url: "https://api.dev.merlot-education.eu/serviceoffering/shapes"
   };
