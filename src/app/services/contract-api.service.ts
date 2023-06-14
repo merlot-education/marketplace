cd772dc64214adc7963be061a526f27b22395915
Marc B <125284318+M-Busk@users.noreply.github.com>
2023-06-14 15:58:57 +0200

    add active role header for contract api requests

--- src/app/services/contract-api.service.ts
+++ src/app/services/contract-api.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
 import { environment } from 'src/environments/environment';
@@ -20,15 +20,21 @@ export class ContractApiService {
 
   constructor(private http: HttpClient, private authService: AuthService) {}
 
+  private getActiveRoleHeaders() : HttpHeaders {
+    let headers = new HttpHeaders();
+    headers = headers.append('Active-Role', this.authService.activeOrganizationRole.value.orgaRoleString);
+    return headers;
+  }
+
   public async createNewContract(offeringId: string, consumerId: string): Promise<IContractDetailed> {
     return await lastValueFrom(this.http.post(environment.contract_api_url, {
       offeringId: offeringId,
       consumerId: consumerId
-    })) as IContractDetailed;
+    }, {headers: this.getActiveRoleHeaders()})) as IContractDetailed;
   }
 
   public async updateContract(contract: IContractDetailed) {
-    return await lastValueFrom(this.http.put(environment.contract_api_url, contract)) as IContractDetailed;
+    return await lastValueFrom(this.http.put(environment.contract_api_url, contract, {headers: this.getActiveRoleHeaders()})) as IContractDetailed;
   }
 
   public async getOrgaContracts(page: number, size: number, consumerId: string): Promise<IPageContracts> {
@@ -38,7 +44,7 @@ export class ContractApiService {
 
   public async getContractDetails(contractId: string): Promise<IContractDetailed> {
     return await lastValueFrom(this.http.get(
-      environment.contract_api_url + "contract/" + contractId)) as IContractDetailed;
+      environment.contract_api_url + "contract/" + contractId, {headers: this.getActiveRoleHeaders()})) as IContractDetailed;
   }
 
   public resolveFriendlyStatusName(contractStatus: string) {
