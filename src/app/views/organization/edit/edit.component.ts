5f4770ca81f8486bbbf54d6943a09b7bb1d6b5d0
Marc B <125284318+M-Busk@users.noreply.github.com>
2023-08-25 11:13:28 +0200

    add fields for federator edit later

--- src/app/views/organization/edit/edit.component.ts
+++ src/app/views/organization/edit/edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IOrganizationData } from "../organization-data";
 import { AuthService } from 'src/app/services/auth.service';
+import { OrganizationsApiService } from 'src/app/services/organizations-api.service';
 
 @Component({
   templateUrl: './edit.component.html',
@@ -10,12 +11,21 @@ export class EditComponent implements OnInit {
 
   protected selectedOrganization: IOrganizationData = null;
 
-  constructor(protected authService: AuthService) {
+  constructor(protected authService: AuthService, 
+    protected organizationsApiService: OrganizationsApiService) {
     authService.activeOrganizationRole.subscribe(orga => {
-      this.selectedOrganization = orga.orgaData;
+      this.selectedOrganization = null;
+      organizationsApiService.getOrgaById(orga.orgaData.selfDescription.verifiableCredential.credentialSubject['@id']).then(result => {
+        this.selectedOrganization = result;
+      })
     })
   }
 
   ngOnInit(): void {
+  }
+
+  saveOrganisationEdit(orga: IOrganizationData) {
+    orga.selfDescription.verifiableCredential.credentialSubject['gax-trust-framework:headquarterAddress'] = orga.selfDescription.verifiableCredential.credentialSubject['gax-trust-framework:legalAddress'];
+    console.log(orga);
   }
 }
