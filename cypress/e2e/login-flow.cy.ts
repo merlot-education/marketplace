a87262f91ca9a0e4f10eaaca378955ff7c9a379a
Martin Jürgens <martin.juergens@capgemini.com>
2023-09-07 16:22:13 +0200

    modify cypress tests

--- cypress/e2e/login-flow.cy.ts
+++ cypress/e2e/login-flow.cy.ts
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-/*beforeEach(() => {
+beforeEach(() => {
   cy.visit('/')
 })
 
@@ -10,17 +10,16 @@ it('testuser can log in', () => {
 
   // click login will redirect to keycloak, use testuser to login
   cy.get("#login-button").click();
-  cy.origin('https://sso.common.merlot-education.eu', () => {
-    cy.get("#username").type("testuser");
-    cy.get("#password").type("testuser");
-    cy.get("#kc-login").click();
-  });
+
+  cy.get("#username").type("testuser");
+  cy.get("#password").type("testuser");
+  cy.get("#kc-login").click();
 
   // at this point we should be redirected again to the MPO and the welcome text should have changed and roles should be visible
-  cy.get("#welcome-text").contains('Willkommen, test user!');
+  cy.get("#welcome-text").contains('Willkommen, Test User!');
   cy.get("#role-select").should("be.visible");
 
   // logout again, after this the welcome text should be for a visitor again
   cy.get("#logout-button").click();
   cy.get("#welcome-text").contains('Willkommen, Besucher!');
-})*/
+})
--- cypress/e2e/view-service-offers.cy.ts
+++ cypress/e2e/view-service-offers.cy.ts
@@ -7,7 +7,3 @@ it('open service offerings', () => {
   cy.contains('Service Angebote erkunden').click()
   cy.url().should('include', 'service-offerings/explore')
 })
-
-it('test that intentionally fails', () => {
-  cy.url().should('include', 'service-offerings/explore')
-})
