055ba03b017706eefd48ea53b545a44639fc2b29
Marc B <125284318+M-Busk@users.noreply.github.com>
2023-10-23 12:58:18 +0200

    remove unused import

--- src/app/services/contract-api.service.ts
+++ src/app/services/contract-api.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
 import { environment } from 'src/environments/environment';
-import { Observable, lastValueFrom } from 'rxjs';
+import { lastValueFrom } from 'rxjs';
 import { IContract, IEdcIdResponse, IEdcNegotiationStatus, IEdcTransferStatus, IPageContracts } from '../views/contracts/contracts-data';
 
 @Injectable({
