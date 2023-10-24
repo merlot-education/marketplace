2363b1880d762e51f5b810f65e388b338a6fbd37
Marc B <125284318+M-Busk@users.noreply.github.com>
2023-10-24 15:26:12 +0200

    working on service offering creation tests

---
+++ cypress/e2e/create-service-offer.cy.ts
@@ -0,0 +1,56 @@
+beforeEach(() => {
+    cy.visit('/')
+    // click login will redirect to keycloak, use testuser to login
+    cy.get("#login-button").click();
+
+    cy.get("#username").type("testuser");
+    cy.get("#password").type("testuser");
+    cy.get("#kc-login").click();
+})
+
+it('create service offering', () => {
+    cy.contains('Service Angebote').click()
+    cy.contains('Service Angebot erstellen').click()
+    cy.url().should('include', 'service-offerings/edit')
+    cy.contains('Art des Service Angebots').next().select("Webanwendung");
+    cy.contains("Füllen Sie das MerlotServiceOfferingSaaS Formular aus");
+    cy.contains("Servicename").next().type("Bla", {force: true});
+    cy.contains("Detaillierte Beschreibung des Services").next().type("Bla", {force: true});
+    cy.contains("Merlot AGB").parent().parent().parent().parent().parent().within(() => {
+        cy.get("button").click({force: true});
+    });
+    cy.contains("Serviceangebotspezifische Geschäftsbedingungen").click({force: true}).parent().parent().parent().parent().parent().within(() => {
+        cy.contains("Link zum Inhalt").next().type("Link", {force: true});
+        cy.contains("Hash des Dokuments").next().type("Hash", {force: true});
+    });
+    cy.contains("Beispielkosten").next().type("Bla", {force: true});
+    cy.contains("Anforderungen an die Hardware").next().type("Bla", {force: true});
+    cy.contains("Nutzeranzahl-Option").parent().parent().parent().parent().parent().within(() => {
+        cy.get("button").click({force: true}).click({force: true}).click({force: true});
+    });
+
+    let userCountOptions = [0, 1, 2, 3];
+
+    cy.get('mat-expansion-panel-header:visible:contains("Nutzeranzahl-Option")').should("have.length", 4).each(($el, index, $list) => {
+        if (index !== 0) {
+            cy.wrap($el).click({force: true});
+        } 
+        cy.wrap($el).parent().parent().parent().contains("Option für maximale Nutzerzahl").next().type(userCountOptions[index] + "", {force: true});
+    });
+
+    cy.contains("Laufzeit-Option").parent().parent().parent().parent().parent().within(() => {
+        cy.get("button").click({force: true}).click({force: true});
+    });
+
+    let runtimeOptions = [0, 1, 2];
+    let runtimeOptionsSelect = ["day(s)", "day(s)", "day(s)"];
+
+    cy.get('mat-expansion-panel-header:visible:contains("Laufzeit-Option")').should("have.length", 3).each(($el, index, $list) => {
+        if (index !== 0) {
+            cy.wrap($el).click({force: true});
+        } 
+        cy.wrap($el).parent().parent().parent().contains("Anzahl-Teil der Laufzeit").next().type(runtimeOptions[index] + "", {force: true});
+        cy.wrap($el).parent().parent().parent().contains("Maß-Teil der Laufzeit").next().select(runtimeOptionsSelect[index]);
+    });
+
+})
