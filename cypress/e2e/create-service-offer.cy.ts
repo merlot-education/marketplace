d9070870f84b81831a1723f3dbcd5200731e2a01
Marc B <125284318+M-Busk@users.noreply.github.com>
2023-10-25 14:51:40 +0200

    confirm data is written properly to the backend

--- cypress/e2e/create-service-offer.cy.ts
+++ cypress/e2e/create-service-offer.cy.ts
@@ -9,6 +9,17 @@ beforeEach(() => {
 })
 
 it('create service offering', () => {
+
+    let offeringName = "DRAFT: Lernplattform (LMS) für betriebliche Weiterbildung (SaaS)";
+    let offeringDescription = "Bewältigen Sie Ihre Enterprise Anforderungen mit der perfekten Kombination aus Learning Management System und Learning Experience Platform. Nutzen Sie Daten über sich ändernde Skill- und Kompetenzanforderungen und den Fachkräftemarkt Ihrer Branche, um Ihre Weiterbildungsprozesse automatisch anzupassen. Dazu ermöglicht das LMS der imc AG als SaaS eine besonders einfache Anbindung an den MERLOT Datenraum, der sichere Verarbeitung und Verwertung von Ihren Daten garantiert und in dem Sie in Kontrolle Ihrer Daten bleiben.";
+    let offeringTncLink = "https://merlot.imc-learning.de/ilp/pages/external-dashboard.jsf?menuId=1501#/?dashboardId=1002"
+    let offeringTncHash = "hash15011002";
+    let offeringCosts = "Abonnement, Verhandlungsbasis";
+    let offeringHWRequirements = "Software as a Service";
+    let userCountOptions = [100, 500, 1000, 0];
+    let runtimeOptions = [1, 2, 0];
+    let runtimeOptionsSelect = ["year(s)", "year(s)", "unlimited"];
+
     cy.contains('Service Angebote').click()
     cy.contains('Service Angebot erstellen').click()
     cy.url().should('include', 'service-offerings/edit')
@@ -17,28 +28,26 @@ it('create service offering', () => {
 
     cy.contains("Änderungen speichern").should("be.disabled");
 
-    cy.contains("Servicename").next().type("Bla", {force: true});
+    cy.contains("Servicename").next().type(offeringName, {force: true});
 
     cy.contains("Service Bereitsteller").next().invoke('val').should("not.be.empty");
     cy.contains("Service Anbieter").next().invoke('val').should("not.be.empty");
 
-    cy.contains("Detaillierte Beschreibung des Services").next().type("Bla", {force: true});
+    cy.contains("Detaillierte Beschreibung des Services").next().type(offeringDescription, {force: true});
     cy.contains("Merlot AGB").scrollIntoView().parent().parent().parent().parent().parent().within(() => {
         cy.get("button").click({force: true});
     });
     cy.contains("Serviceangebotspezifische Geschäftsbedingungen").scrollIntoView().click({force: true}).parent().parent().parent().parent().parent().within(() => {
-        cy.contains("Link zum Inhalt").next().type("Link", {force: true});
-        cy.contains("Hash des Dokuments").next().type("Hash", {force: true});
+        cy.contains("Link zum Inhalt").next().type(offeringTncLink, {force: true});
+        cy.contains("Hash des Dokuments").next().type(offeringTncHash, {force: true});
     });
-    cy.contains("Beispielkosten").next().type("Bla", {force: true});
-    cy.contains("Anforderungen an die Hardware").next().type("Bla", {force: true});
+    cy.contains("Beispielkosten").next().type(offeringCosts, {force: true});
+    cy.contains("Anforderungen an die Hardware").next().type(offeringHWRequirements, {force: true});
     cy.contains("Nutzeranzahl-Option").scrollIntoView().parent().parent().parent().parent().parent().within(() => {
         cy.get("button").click({force: true}).click({force: true}).click({force: true});
     });
 
-    let userCountOptions = [0, 1, 2, 3];
-
-    cy.get('mat-expansion-panel-header:visible:contains("Nutzeranzahl-Option")').should("have.length", 4).each(($el, index, $list) => {
+    cy.get('mat-expansion-panel-header:visible:contains("Nutzeranzahl-Option")').should("have.length", userCountOptions.length).each(($el, index, $list) => {
         if (index !== 0) {
             cy.wrap($el).click({force: true});
         } 
@@ -49,10 +58,7 @@ it('create service offering', () => {
         cy.get("button").click({force: true}).click({force: true});
     });
 
-    let runtimeOptions = [0, 1, 2];
-    let runtimeOptionsSelect = ["day(s)", "day(s)", "day(s)"];
-
-    cy.get('mat-expansion-panel-header:visible:contains("Laufzeit-Option")').should("have.length", 3).each(($el, index, $list) => {
+    cy.get('mat-expansion-panel-header:visible:contains("Laufzeit-Option")').should("have.length", runtimeOptions.length).each(($el, index, $list) => {
         if (index !== 0) {
             cy.wrap($el).click({force: true});
         } 
@@ -74,6 +80,34 @@ it('create service offering', () => {
             cy.contains("Details").click({force: true});
         });
         cy.contains("Details zum Service Angebot").parent().parent().within(() => {
+            cy.contains("Angebotstyp").parent().should("include.text", "Webanwendung");
+            cy.contains("Service ID").parent().should("include.text", offeringId);
+            cy.contains("Name").parent().should("include.text", offeringName);
+            cy.contains("Erstelldatum");
+            cy.contains("Anbieter");
+            cy.contains("Status").parent().should("include.text", "In Bearbeitung");
+            cy.contains("Beschreibung").parent().should("include.text", offeringDescription);
+            cy.contains("Letzte Änderung");
+            cy.contains("Beispielkosten").parent().should("include.text", offeringCosts);
+            cy.contains("Serviceangebotspezifische Geschäftsbedingungen").parent().should("include.text", offeringTncLink).should("include.text", offeringTncHash);
+            cy.contains("Hardware-Anforderungen").parent().should("include.text", offeringHWRequirements);
+            for (let userOpt of userCountOptions) {
+                if (userOpt !== 0) {
+                    cy.contains("Optionen für Anzahl erlaubter Benutzer").parent().should("include.text", userOpt);
+                } else {
+                    cy.contains("Optionen für Anzahl erlaubter Benutzer").parent().should("include.text", "Unbegrenzt");
+                }
+            }
+
+            for (let runtimeOptIdx = 0; runtimeOptIdx < runtimeOptions.length; runtimeOptIdx++) {
+                if (runtimeOptions[runtimeOptIdx] !== 0 && runtimeOptionsSelect[runtimeOptIdx] !== "unlimited") {
+                    cy.contains("Laufzeitoptionen").parent().should("include.text", runtimeOptions[runtimeOptIdx] + " " + runtimeOptionsSelect[runtimeOptIdx]);
+                } else {
+                    cy.contains("Laufzeitoptionen").parent().should("include.text", "Unbegrenzt");
+                }
+            }
+            
+
             cy.contains("Schließen");
             cy.contains("Veröffentlichen");
             cy.contains("Löschen").scrollIntoView().click({force: true});
