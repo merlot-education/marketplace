07133f7192f1178a93aeb6d85faa1b80cd12537a
Marc B <125284318+M-Busk@users.noreply.github.com>
2023-10-25 15:40:51 +0200

    extend test to check as visitor, add comments

--- cypress/e2e/create-service-offer.cy.ts
+++ cypress/e2e/create-service-offer.cy.ts
@@ -1,15 +1,23 @@
 beforeEach(() => {
     cy.visit('/')
+
+    //open merlot marketplace landing page, user is not logged in, use the welcome text to check that user is a visitor
+    cy.get("#welcome-text").contains('Willkommen, Besucher!');
+
     // click login will redirect to keycloak, use testuser to login
     cy.get("#login-button").click();
 
     cy.get("#username").type("testuser");
     cy.get("#password").type("testuser");
     cy.get("#kc-login").click();
+
+    // make sure welcome text changed
+    cy.get("#welcome-text").contains('Willkommen, Test User!');
 })
 
 it('create service offering', () => {
 
+    // prepare some data to fill in the form
     let offeringName = "DRAFT: Lernplattform (LMS) für betriebliche Weiterbildung (SaaS)";
     let offeringDescription = "Bewältigen Sie Ihre Enterprise Anforderungen mit der perfekten Kombination aus Learning Management System und Learning Experience Platform. Nutzen Sie Daten über sich ändernde Skill- und Kompetenzanforderungen und den Fachkräftemarkt Ihrer Branche, um Ihre Weiterbildungsprozesse automatisch anzupassen. Dazu ermöglicht das LMS der imc AG als SaaS eine besonders einfache Anbindung an den MERLOT Datenraum, der sichere Verarbeitung und Verwertung von Ihren Daten garantiert und in dem Sie in Kontrolle Ihrer Daten bleiben.";
     let offeringTncLink = "https://merlot.imc-learning.de/ilp/pages/external-dashboard.jsf?menuId=1501#/?dashboardId=1002"
@@ -20,19 +28,24 @@ it('create service offering', () => {
     let runtimeOptions = [1, 2, 0];
     let runtimeOptionsSelect = ["year(s)", "year(s)", "unlimited"];
 
+    // click on navigation entry Serviceangebote,  the submenu is extended
     cy.contains('Service Angebote').click()
+
+    // click on navigation entry Serviceangebot erstellen, the form will be displayed on the right side of the screen
     cy.contains('Service Angebot erstellen').click()
     cy.url().should('include', 'service-offerings/edit')
+
+    // select Webanwendung as type
     cy.contains('Art des Service Angebots').next().should("not.be.empty").select("Webanwendung");
     cy.contains("Füllen Sie das MerlotServiceOfferingSaaS Formular aus");
 
+    // make sure we cannot submit the form yet
     cy.contains("Änderungen speichern").should("be.disabled");
 
+    // fill the form fields as specified above
     cy.contains("Servicename").next().type(offeringName, {force: true});
-
     cy.contains("Service Bereitsteller").next().invoke('val').should("not.be.empty");
     cy.contains("Service Anbieter").next().invoke('val').should("not.be.empty");
-
     cy.contains("Detaillierte Beschreibung des Services").scrollIntoView().next().type(offeringDescription, {force: true});
     cy.contains("Merlot AGB").scrollIntoView().parent().parent().parent().parent().parent().within(() => {
         cy.get("button").click({force: true});
@@ -46,18 +59,15 @@ it('create service offering', () => {
     cy.contains("Nutzeranzahl-Option").scrollIntoView().parent().parent().parent().parent().parent().within(() => {
         cy.get("button").click({force: true}).click({force: true}).click({force: true});
     });
-
     cy.get('mat-expansion-panel-header:visible:contains("Nutzeranzahl-Option")').should("have.length", userCountOptions.length).each(($el, index, $list) => {
         if (index !== 0) {
             cy.wrap($el).click({force: true});
         } 
         cy.wrap($el).parent().parent().parent().contains("Option für maximale Nutzerzahl").next().type(userCountOptions[index] + "", {force: true});
     });
-
     cy.contains("Laufzeit-Option").scrollIntoView().parent().parent().parent().parent().parent().within(() => {
         cy.get("button").click({force: true}).click({force: true});
     });
-
     cy.get('mat-expansion-panel-header:visible:contains("Laufzeit-Option")').should("have.length", runtimeOptions.length).each(($el, index, $list) => {
         if (index !== 0) {
             cy.wrap($el).click({force: true});
@@ -65,38 +75,57 @@ it('create service offering', () => {
         cy.wrap($el).parent().parent().parent().contains("Anzahl-Teil der Laufzeit").next().type(runtimeOptions[index] + "", {force: true});
         cy.wrap($el).parent().parent().parent().contains("Maß-Teil der Laufzeit").next().select(runtimeOptionsSelect[index]);
     });
-
     cy.contains("Erstelldatum").next().invoke('val').should("not.be.empty");
-
     cy.contains("Merlot AGB akzeptieren").parent().within(() => {
         cy.get("input").check();
     });
 
+
+    // make sure save button is no longer disabled
+    // click on button "Änderungen speichern", the response that the offer is stored will be shown
     cy.contains("Änderungen speichern").should("not.be.disabled").click();
     cy.contains("Selbstbeschreibung erfolgreich gespeichert!", {timeout: 30000}).should("include.text", "ServiceOffering:").then((result) => {
+
+        // store id of created offering
         let offeringId = result.get(0).innerText.match(/ServiceOffering:\S+/)[0];
+
+        // click on navigation entry Angebote erkunden, the created offer is shown on top of the page
         cy.contains("Service Angebote erkunden").click();
+
+        // search for offering in list
         cy.contains(offeringId).parent().parent().parent().within(() => {
+            //check status of created offer equals "In Bearbeitung"
             cy.contains("Status").parent().should("include.text", "In Bearbeitung");
+            // click on button Bearbeiten of the created offer
             cy.contains("Bearbeiten").click({force: true});
         });
 
+        // a popup opens with the form to modify offer (here data could be checked with entered ones)
         cy.contains("Service Angebot \"" + offeringName + "\" bearbeiten").parent().parent().within(() => {
+            //change field Servicename* to Lernplattform (LMS) für betriebliche Weiterbildung (SaaS) 
             offeringName = "Lernplattform (LMS) für betriebliche Weiterbildung (SaaS)";
             cy.contains("Servicename").next().clear({force: true});
             cy.contains("Servicename").next().type(offeringName, {force: true});
+
+            // scroll down and click on button "Publish", the respones that the offer is published will be shown
             cy.contains("Veröffentlichen").scrollIntoView().click({force: true});
             cy.contains("Selbstbeschreibung erfolgreich gespeichert!");
             cy.contains("Schließen").click();
         });
 
+        // activate filter, select Veröffentlicht, the offer should be the first entry of the list of published offers
         cy.contains("Zeige nur Angebote mit Status").parent().next().next().select("Veröffentlicht", {force: true});
         cy.contains("Zeige nur Angebote mit Status").prev().click({force: true});
 
+        // search for offering in list
         cy.contains(offeringId).parent().parent().parent().within(() => {
+            // check status of published offer equals "Veröffentlicht"
             cy.contains("Status").parent().should("include.text", "Veröffentlicht");
+            // click on the card button "Details" to open the detail page
             cy.contains("Details").click({force: true});
         });
+
+        //double-check the saved values with the expected ones
         cy.contains("Details zum Service Angebot").parent().parent().within(() => {
             cy.contains("Angebotstyp").parent().should("include.text", "Webanwendung");
             cy.contains("Service ID").parent().should("include.text", offeringId);
@@ -125,7 +154,61 @@ it('create service offering', () => {
                 }
             }
             
-            cy.contains("Schließen");
+            cy.contains("Schließen").scrollIntoView().click({force: true});;
+        });
+
+        // click on logout, the user is redirected to the MPO landingpage, use the welcome text to check that user is a visitor
+        cy.get("#logout-button").click();
+        cy.get("#welcome-text").contains('Willkommen, Besucher!');
+
+        // click on navigation entry Serviceangebote,  the submenu is extended
+        cy.contains('Service Angebote').click()
+
+        // click on navigation entry Serviceangebot erkunden, the form will be displayed on the right side of the screen
+        cy.contains('Service Angebote erkunden').click()
+        cy.url().should('include', 'service-offerings/explore')
+
+        // search for offering in list
+        cy.contains(offeringId).parent().parent().parent().parent().parent().within(() => {
+            // make sure type and name is right
+            cy.contains("Webanwendung");
+            cy.contains(offeringName);
+
+            // check for existence of fields
+            cy.contains("Erstelldatum");
+            cy.contains("Anbieter");
+
+            // visitors have no details button
+        });
+
+        // log back in as testuser
+        // click login will redirect to keycloak, use testuser to login
+        cy.get("#login-button").click();
+
+        cy.get("#username").type("testuser");
+        cy.get("#password").type("testuser");
+        cy.get("#kc-login").click();
+
+        // make sure welcome text changed
+        cy.get("#welcome-text").contains('Willkommen, Test User!');
+
+        // click on navigation entry Serviceangebote,  the submenu is extended
+        cy.contains('Service Angebote').click()
+
+        // click on navigation entry Serviceangebot erkunden, the form will be displayed on the right side of the screen
+        cy.contains('Service Angebote erkunden').click()
+        cy.url().should('include', 'service-offerings/explore')
+
+        // search for offering in list
+        cy.contains(offeringId).parent().parent().parent().within(() => {
+            // check status of published offer equals "Veröffentlicht"
+            cy.contains("Status").parent().should("include.text", "Veröffentlicht");
+            // click on the card button "Details" to open the detail page
+            cy.contains("Details").click({force: true});
+        });
+
+        // delete the offering
+        cy.contains("Details zum Service Angebot").parent().parent().within(() => {
             cy.contains("Angebot neu erstellen");
             cy.contains("Widerrufen").scrollIntoView().click({force: true});
             cy.contains("Status").parent().should("include.text", "Widerrufen");
@@ -133,6 +216,8 @@ it('create service offering', () => {
             cy.contains("Status").parent().should("include.text", "Gelöscht");
             cy.contains("Endgültig löschen").scrollIntoView().click({force: true});
         });
+
+        // make sure the offering is no longer in the list
         cy.get("c-card-body").contains(offeringId).should("not.exist");
     });
 })
