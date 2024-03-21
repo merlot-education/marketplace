41a2a1376f325ce7d2366b8b675fd14ba56ce031
Marc B <125284318+M-Busk@users.noreply.github.com>
2024-03-21 17:26:06 +0100

    update button appearance, also add to offerings

--- src/app/views/serviceofferings/explore/explore.component.ts
+++ src/app/views/serviceofferings/explore/explore.component.ts
@@ -310,28 +310,31 @@ export class ExploreComponent implements OnInit, OnDestroy {
     this.jsonViewHidden = !this.jsonViewHidden;
   }
 
-  downloadJsonFile() {
-    // Convert the object to a JSON string
-    const jsonData = JSON.stringify(this.selectedOfferingDetails.selfDescription, null, 2);
+  downloadJsonFile(id: string) {
+    this.serviceOfferingApiService.fetchServiceOfferingDetails(id).then(result => {
+      let selfDescription = result.selfDescription;
+      // Convert the object to a JSON string
+      const jsonData = JSON.stringify(selfDescription, null, 2);
 
-    // Create a Blob from the JSON string
-    const blob = new Blob([jsonData], { type: 'application/json' });
+      // Create a Blob from the JSON string
+      const blob = new Blob([jsonData], { type: 'application/json' });
 
-    // Create a URL for the Blob
-    const url = window.URL.createObjectURL(blob);
+      // Create a URL for the Blob
+      const url = window.URL.createObjectURL(blob);
 
-    // Create an anchor element with download attribute
-    const a = document.createElement('a');
-    a.href = url;
-    const id = this.selectedOfferingDetails.selfDescription.verifiableCredential.credentialSubject['@id'];
-    a.download = 'selfdescription_' + id + '.json';
+      // Create an anchor element with download attribute
+      const a = document.createElement('a');
+      a.href = url;
+      const id = selfDescription.verifiableCredential.credentialSubject["@id"];
+      a.download = 'selfdescription_' + id + '.json';
 
-    // Programmatically click the anchor element to trigger the download
-    document.body.appendChild(a);
-    a.click();
+      // Programmatically click the anchor element to trigger the download
+      document.body.appendChild(a);
+      a.click();
 
-    // Clean up
-    window.URL.revokeObjectURL(url);
-    document.body.removeChild(a);
+      // Clean up
+      window.URL.revokeObjectURL(url);
+      document.body.removeChild(a);
+    })
   }
 }
