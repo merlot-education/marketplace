e66c513c5dab44abf69e451bea5342ca7985b4e3
Marc B <125284318+M-Busk@users.noreply.github.com>
2024-04-26 16:47:07 +0200

    switch to demo AAS on dev environment for testing

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -5,7 +5,7 @@ export const environment = {
     serviceoffering_api_url: "https://api.dev.merlot-education.eu/serviceoffering/",
     contract_api_url: "https://api.dev.merlot-education.eu/contract/",
     wizard_api_url: "https://api.dev.merlot-education.eu/serviceoffering/shapes",
-    login_authority_url: "https://auth-service.dev.merlot-education.eu",
+    login_authority_url: "https://auth-service.demo.merlot-education.eu",
     login_client_id: "MPO",
     daps_server_url: 'https://daps.dev.merlot-education.eu/auth',
     daps_audience: 'idsc:IDS_CONNECTORS_ALL'
